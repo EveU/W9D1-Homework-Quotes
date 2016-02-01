9950e88337ec31963bd547acdc499f4265d8a4b9
Evelyn Utterson <e.utterson@hotmail.co.uk>
2016-02-01 19:24:36 +0000

    quote created at real time

--- public/app.js
+++ public/app.js
@@ -1,40 +1,13 @@
 // Quote Lab
 // The quotes are all hardcoded in our web page. Let's start moving towards having a dynamic web application and set the existing quotes programatically via Javascript.
 
-// Create an array of the existing quotes, each quote having text and author
-// Add the quotes to the page dynamically with Javascript
-// Further:
-
-// Extend our quote application so that a user can add quotes. (Don't worry about these saving between refreshing page, will do this tomorrow)
 // Even Further:
-
 // Add functionality to delete quotes
 // Style page
-// Even Even Further:
-
-// While typing show the quote being created at real time.
 
 var init = function(){
 
-  var addNewQuote = function(){
-    var blockquote = document.createElement('blockquote');
-    blockquote.innerText = quoteArray[i].text;
-    var cite = document.createElement('cite');
-    cite.innerText = quoteArray[i].author;
-    blockquote.appendChild(cite);
-    newQuote.appendChild(blockquote);
-    quoteSection.appendChild(newQuote);
-  }
-
-  // var quotesArray = [];
-  // var quotes = document.getElementsByClassName('quote');
-  // for(i=0;i<quotes.length;i++){
-  //   var quoteText = quotes[i].children[0].innerText;
-  //   var quoteAuthor = quotes[i].children[0].children[0].innerText;
-  //   quotesArray.push({quote: quoteText, author: quoteAuthor});
-  // }
-  // console.log(quotesArray);
-
+  // Create an array of the existing quotes, each quote having text and author
   var quoteArray = [
     {
       text:"Visual Basic is the way forward, I don't know why we are doing Javascript.",
@@ -58,6 +31,17 @@ var init = function(){
     }
   ]
 
+  // Add the quotes to the page dynamically with Javascript
+  var addNewQuote = function(){
+    var blockquote = document.createElement('blockquote');
+    blockquote.innerText = quoteArray[i].text;
+    var cite = document.createElement('cite');
+    cite.innerText = quoteArray[i].author;
+    blockquote.appendChild(cite);
+    newQuote.appendChild(blockquote);
+    quoteSection.appendChild(newQuote);
+  }
+
   var quoteSection = document.getElementsByClassName('quote-section')[0];
   var newQuote = document.createElement('article');
   newQuote.classList.add("quote");
@@ -66,6 +50,8 @@ var init = function(){
     addNewQuote();
   }
 
+  // Further:
+  // Extend our quote application so that a user can add quotes. (Don't worry about these saving between refreshing page, will do this tomorrow)
   var quoteForm = document.createElement("form");
   var quoteFormLabel = document.createElement("label");
   quoteFormLabel.innerText = "Add Quote:"
@@ -106,10 +92,36 @@ var init = function(){
     blockquote.appendChild(cite);
     newQuote.appendChild(blockquote);
     quoteSection.appendChild(newQuote);
+    // blockquote.innerText = "";
+    document.getElementById('quote-text-input').value = "";
+    document.getElementById('quote-author-input').value = "";
   };
   
   button.onclick = handleClick;
 
+  // Even Even Further:
+  // While typing show the quote being created at real time.
+  var blockquote = document.createElement('blockquote');
+  var cite = document.createElement('cite');
+  
+  var whileTyping = function(){
+    var newQuoteText = document.getElementById('quote-text-input').value;
+    var newQuoteAuthor = document.getElementById('quote-author-input').value;
+    var newQuote = document.createElement('article');
+    newQuote.classList.add("quote");
+    newQuoteText = document.getElementById('quote-text-input').value;
+    newQuoteAuthor = document.getElementById('quote-author-input').value;
+    blockquote.innerText = newQuoteText;
+    cite.innerText = newQuoteAuthor;
+    blockquote.appendChild(cite);
+    newQuote.appendChild(blockquote);
+    quoteSection.appendChild(newQuote);
+  }
+
+  var input = document.getElementById('quote-text-input');
+  input.onkeyup = whileTyping;
+  var input2 = document.getElementById('quote-author-input');
+  input2.onkeyup = whileTyping;
 
   // var quoteArticle = document.createElement("article");
   // quoteArticle.classList.add("quote");
@@ -156,3 +168,13 @@ var init = function(){
 }
 
 window.onload = init;
+
+
+  // var quotesArray = [];
+  // var quotes = document.getElementsByClassName('quote');
+  // for(i=0;i<quotes.length;i++){
+  //   var quoteText = quotes[i].children[0].innerText;
+  //   var quoteAuthor = quotes[i].children[0].children[0].innerText;
+  //   quotesArray.push({quote: quoteText, author: quoteAuthor});
+  // }
+  // console.log(quotesArray);
